0d6bedbbabc9a0a33ccd03c9f547e58f4f725949
Robin <romarcelo@cambridge.org>
2020-03-03 12:00:44 +0800

    Completed end product

--- end/session_02/lib/screens/home_screen.dart
+++ end/session_02/lib/screens/home_screen.dart
@@ -1,24 +1,62 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedTabItem = 0;
+
+  static const List<Widget> _tabScreens = <Widget>[
+    Text('Talks'),
+    Text('Booths'),
+    Text('Streaming'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedTabItem = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Route'),
+        actions: <Widget>[trailingActions(context)],
       ),
       body: Container(
         alignment: Alignment.center,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('Home Page'),
-            RaisedButton(
-                child: Text('move'),
-                onPressed: () => Navigator.pushNamed(context, '/about')),
+            _tabScreens.elementAt(_selectedTabItem),
           ],
         ),
       ),
+      bottomNavigationBar: createBottomNav(),
+    );
+  }
+
+  IconButton trailingActions(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.help_outline),
+      onPressed: () => Navigator.pushNamed(context, '/about'),
+    );
+  }
+
+  BottomNavigationBar createBottomNav() {
+    return BottomNavigationBar(
+      currentIndex: _selectedTabItem,
+      onTap: _onItemTapped,
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.group), title: Text('Talks')),
+        BottomNavigationBarItem(icon: Icon(Icons.place), title: Text('Booths')),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.ondemand_video), title: Text('Streaming')),
+      ],
     );
   }
 }
